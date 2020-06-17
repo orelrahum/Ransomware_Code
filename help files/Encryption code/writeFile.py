09d6e71bff020a0e83cfc040df22fb0195e437f1
orelrahum <44848947+orelrahum@users.noreply.github.com>
2020-06-17 20:36:28 +0300

    a

--- help files/Encryption code/writeFile.py
+++ help files/Encryption code/writeFile.py
@@ -1,15 +1,9 @@
+def main():
+    for i in range(150):
+        nameFile= "{}.txt ".format(i)
+        writeFile = open(nameFile, "w")
+        writeFile.close()
 
 
-with open("fileProject.txt", "r") as fp:
-  for i in range(10000):
-    line = fp.readline()
-    nameFile= "file{}.txt ".format(i)
-    writeFile = open(nameFile, "a")
-    writeFile.write(line)
-
-     #writeFile.write(fp.readline())
-writeFile.close()
-
-#open and read the file after the appending:
-# f = open("fileProject.txt", "r")
-# print(f.read())
+if __name__ == "__main__":
+    main()
