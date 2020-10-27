74cb1d3f5be6718d9b45caf298e0d718c316bdb0
orelrahum <44848947+orelrahum@users.noreply.github.com>
2020-10-27 15:14:16 +0200

    git

--- My Project/Unencrypted files/main.py
+++ My Project/Unencrypted files/main.py
@@ -7,35 +7,32 @@ global name_file
 def create_file():
     global name_file
     name_file = "output files\\Unencrypted{}.txt ".format(number_files)
-    write_file = open(name_file, "w+")
-    write_file.close()
+    create_files = open(name_file, "a+")
+    create_files.close()
 
 
 if __name__ == "__main__":
-    total = 0
-    edit_file = "Big files to edit\\alice29.txt"  # Please enter here your file name
-    num_of_lines = random.randint(50, 100)
-    print(num_of_lines)
-    num_total_lines = sum(1 for line in open(edit_file))
-    print(num_total_lines)
     global number_files
+    edit_file = "Big files to edit\\total.txt"  # Please enter here your file name
+    num_of_lines = random.randint(100, 200)
+    num_total_lines = sum(1 for line in open(edit_file))
     number_files = 1
     x = 0
     file = open(edit_file, "r")
-    for lines in file:
+    for lines in file.readlines():
         create_file()
         if x < num_of_lines:
             write_file = open(name_file, "a+")
             write_file.write(lines)
+            print(lines)
+            write_file.close()
         if x == num_of_lines:
             num_total_lines = num_total_lines - num_of_lines
-            num_of_lines = random.randint(50, 100)
-            total = total+num_of_lines
-            print(total)
+            num_of_lines = random.randint(100, 200)
             if num_total_lines <= num_of_lines:
                 num_of_lines = num_total_lines
             number_files = number_files + 1
             x = 0
-        x = x + 1
+        x = x+1
     file.close()
 
